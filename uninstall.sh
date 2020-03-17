ee88f1290b2e675ba6f721f8c260380a18cab7fe
yoopersteeze <ryan.peterson1@icloud.com>
2020-03-16 22:20:07 -0500

    updated readme

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Unloading LaunchDaemon"
 launchctl unload -w /Library/LaunchDaemons/com.breaktaker.launcher.plist
+echo "Removing LaunchDaemon, Script and Log file"
 rm -rf /Library/Application\ Support/BreakTaker/
 rm /var/tmp/breaktaker.log
 rm /Library/LaunchDaemons/com.breaktaker.launcher.plist
