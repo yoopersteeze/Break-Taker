e0d2c78ba80e7922673a83538451fa26db81fbf0
yoopersteeze <ryan.peterson1@icloud.com>
2020-03-18 10:46:02 -0500

    updated uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+
+log="/var/tmp/breaktaker.log"
 echo "Unloading LaunchDaemon"
 launchctl unload -w /Library/LaunchDaemons/com.breaktaker.launcher.plist
-echo "Removing LaunchDaemon, Script and Log file"
+echo "Removing LaunchDaemon and Script."
 rm -rf /Library/Application\ Support/BreakTaker/
-rm /var/tmp/breaktaker.log
 rm /Library/LaunchDaemons/com.breaktaker.launcher.plist
+if [[ ! -f $log ]]; then
+  echo "No log file found!"
+else
+  rm $log
+fi
 echo "Done"
 exit 0
